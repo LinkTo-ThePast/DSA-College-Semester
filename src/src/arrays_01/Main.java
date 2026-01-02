865edaf1fa7909ce65df815c968131754c1b3e4e
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-01-02 00:20:09 -0600

    lesson 01(remove duplicates): fixing two for cycle

--- src/src/arrays_01/Main.java
+++ src/src/arrays_01/Main.java
@@ -1,11 +1,10 @@
 package arrays_01;
 
 public class Main {
-    // testing arrays
-
-    RemoveDuplicates removeDuplicates = new RemoveDuplicates();
 
     public static void main(String[] args) {
-
+        RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+        int[] numsTest1 = removeDuplicates.numsTest2;
+        System.out.println(removeDuplicates.getSizeOfUniqueElements(numsTest1));
     }
 }
--- src/src/arrays_01/RemoveDuplicates.java
+++ src/src/arrays_01/RemoveDuplicates.java
@@ -5,12 +5,31 @@ public class RemoveDuplicates {
     int[] nums;
     //
     int[] numsTest = {0,0,1,1};
+
+    //
+    int[] numsTest2 = {0,0,1,1,1,2,2,3,3,4};
     // return number of unique elements
     int k;
 
-    private int getSizeOfUniqueElements(int[] nums) {
+    public int getSizeOfUniqueElements(int[] nums) {
         k = 0;
         for (int i = 0; i < nums.length; i++ ) {
+            // first iteration:
+            // i = 0 --> nums[i=0] = 0;
+            // j = i + 1 = 1 --> nums[j=1] = 0;
+
+            // second iteration:
+            // i = 1 ---> nums[i=1] = 0;
+            // j = i + 1 = 2 ---> nums[j=2] = 1:
+
+            // third iteration
+            // i = 2 ----> nums[i=2] = 1;
+            // j = i + 1 = 3 ---> nums[j=3] = 1;
+
+            // fourth iteration
+            // i = 3 --> = 1
+            // j
+
             for (int j = i + 1 ; j < nums.length; j++) {
                 if (nums[i] == nums[j]) {
                     k++;
